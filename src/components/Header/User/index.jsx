b93a10a73d497c4b8894562b8c0c444cc0c9e201
Bali <lisiyuan@tengyue360.com>
2018-12-07 13:37:41 +0800

    修改不存在侧边栏取消选中状态问题

--- src/components/Header/User/index.jsx
+++ src/components/Header/User/index.jsx
@@ -44,6 +44,7 @@ class User extends Component {
 		);
 	}
 	handleClick(command) {
+        console.log(this.props.router.defaultActive)
         switch (command) {
             case 'setUp': 
                 window.location.href = '#/SetUp';
