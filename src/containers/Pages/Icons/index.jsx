ddaf8df2c392052e02385da1d15be590598b5ad4
Bali <lisiyuan@tengyue360.com>
2018-12-11 19:03:50 +0800

    添加复制剪切板功能

--- src/containers/Pages/Icons/index.jsx
+++ src/containers/Pages/Icons/index.jsx
@@ -4,24 +4,37 @@
  */
 
 import React, { Component } from 'react';
+import { Message } from 'element-react';
 
-import iconList from './iconLIst'
+import iconList from './iconLIst';
 
-import './style.scss'
+import './style.scss';
 
 export default class Icons extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		};
+	}
 	render() {
 		return (
 			<div className="admin__icons">
 				<p>element-react提供的图标并不多，添加一些在这里</p>
-                
-                <pre style={{padding: "10px", margin: "10px 0", background: "#f8f8f8"}}>
-                    &lt;i className="admin-icon-tiaoshi"/&gt;
-                </pre>
+				<input
+					style={{ position: 'absolute', zIndex: '-9999', opacity: 0 }}
+					ref="input"
+					value={this.state.value}
+					type="text"
+					readOnly
+				/>
+				<pre style={{ padding: '10px', margin: '10px 0', background: '#f8f8f8' }}>
+					&lt;i className="admin-icon-tiaoshi"/&gt;
+				</pre>
 
 				<ul className="admin__icon__list">
 					{iconList.map((v, i) => (
-						<li key={i}>
+						<li style={{ cursor: 'pointer' }} key={i} onClick={this.copy.bind(this, v)}>
 							<span>
 								<i className={v} />
 								{v}
@@ -30,6 +43,24 @@ export default class Icons extends Component {
 					))}
 				</ul>
 			</div>
+		);
+	}
+	copy(val) {
+		this.setState(
+			{
+				value: `<i className="${val}"/>`
+			},
+			() => {
+				this.refs.input.select();
+				if (document.execCommand('copy')) {
+					document.execCommand('copy');
+					Message({
+						showClose: true,
+						message: '代码已经成功复制到剪切板',
+						type: 'success'
+					});
+				}
+			}
 		);
 	}
 }
