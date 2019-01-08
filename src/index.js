43a127a4a622218a53020079cffa841523b62747
Bali <lisiyuan@tengyue360.com>
2019-01-08 14:08:45 +0800

    删除多余代码

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import App from '@/containers/App'
 
 import './index.css';
 // import * as serviceWorker from './serviceWorker';
-// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Provider {...store}><App/></Provider>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
